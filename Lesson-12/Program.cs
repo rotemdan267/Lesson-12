5ee516201db769d5f4ea76766d0b42a7bfd024dd
rotemdan267 <rotemdan267@gmail.com>
2021-08-04 01:06:59 +0300

    Add project files.

---
+++ Lesson-12/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Table
+    {
+        public int Price { get; private set; }
+        public string Color { get; set; }
+        public string Manufacture { get; }
+    }
+}
